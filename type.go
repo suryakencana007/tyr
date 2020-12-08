dc203a5fce315f7d98b042bef97e25dedfa78f60
suryakencana007 <nanang.jobs@gmail.com>
2020-12-09 01:23:02 +0700

    testing null time character -

--- type.go
+++ type.go
@@ -135,6 +135,7 @@ func (nt NullTime) MarshalJSON() ([]byte, error) {
 		return []byte("null"), nil
 	}
 	val := fmt.Sprintf("%s", nt.Time.Format(time.RFC3339))
+	fmt.Printf(">>>>> log tyr %s\n", val)
 	return []byte(fmt.Sprintf("%s", val)), nil
 }
 
